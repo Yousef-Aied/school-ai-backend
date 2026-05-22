3a61b750f3b038e153505f0989d01f8ed566bb6c
Yousef Aied <yousefaied71@gmail.com>
2026-05-22 14:22:14 +0300

    fix: json mapping for chat

--- Models/ChatRequest.cs
+++ Models/ChatRequest.cs
@@ -1,10 +1,22 @@
-﻿namespace SchoolPlatform.Api.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SchoolPlatform.Api.Models
 {
     public class ChatRequest
     {
+        [JsonPropertyName("conversation_id")]
         public string ConversationId { get; set; } = "";
+
+        [JsonPropertyName("message")]
         public string Message { get; set; } = "";
+
+        [JsonPropertyName("student_id")]
         public int StudentId { get; set; }
+
+        [JsonPropertyName("grade")]
+        public string Grade { get; set; } = "";
+
+        [JsonPropertyName("subject")]
         public string Subject { get; set; } = "";
     }
 }
